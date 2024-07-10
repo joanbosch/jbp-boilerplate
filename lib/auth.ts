d533fbf007bc860940d84587291330d92c0ea337
Joan Bosch <joanboschpons@gmail.com>
2024-07-10 13:01:49 +0200

    initial implementation of auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,18 +1,19 @@
 import NextAuth from "next-auth";
-import type { NextAuthOptions } from "next-auth";
+import type { NextAuthConfig } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
+import Nodemailer from "next-auth/providers/nodemailer"
+
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import config from "@/config";
-import connectMongo from "./mongo";
+import connectMongo from "@/lib/mongo";
 
-interface NextAuthOptionsExtended extends NextAuthOptions {
+interface NextAuthOptionsExtended extends NextAuthConfig {
   adapter: any;
 }
 
 export const authOptions: NextAuthOptionsExtended = {
   // Set any random key in .env.local
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.AUTH_SECRET,
   providers: [
     GoogleProvider({
       // Follow the "Login with Google" tutorial to get your credentials
@@ -32,21 +33,20 @@ export const authOptions: NextAuthOptionsExtended = {
     // Requires a MongoDB database. Set MONOGODB_URI env variable.
     ...(connectMongo
       ? [
-          EmailProvider({
+         Nodemailer({
             server: process.env.EMAIL_SERVER,
-            from: config.mailgun.fromNoReply,
+            from: config.resend.fromNoReply,
           }),
         ]
       : []),
   ],
   // New users will be saved in Database (MongoDB Atlas). Each user (model) has some fields like name, email, image, etc..
   // Requires a MongoDB database. Set MONOGODB_URI env variable.
-  // Learn more about the model type: https://next-auth.js.org/v3/adapters/models
-  ...(connectMongo && { adapter: MongoDBAdapter(connectMongo) }),
+  ...(connectMongo ? { adapter: MongoDBAdapter(connectMongo) } : {adapter: undefined}),
 
   callbacks: {
     session: async ({ session, token }) => {
-      if (session?.user) {
+      if (session?.user && token?.sub) {
         session.user.id = token.sub;
       }
       return session;
@@ -56,11 +56,11 @@ export const authOptions: NextAuthOptionsExtended = {
     strategy: "jwt",
   },
   theme: {
-    brandColor: config.colors.main,
+    brandColor: 'light',
     // Add you own logo below. Recommended size is rectangle (i.e. 200x50px) and show your logo + name.
     // It will be used in the login flow to display your logo. If you don't add it, it will look faded.
     logo: `https://${config.domainName}/logoAndName.png`,
-  },
+  }
 };
 
 export default NextAuth(authOptions);
