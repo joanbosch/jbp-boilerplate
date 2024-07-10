058f197a67bf8830be35ae1bb22314851b7d6ce0
Joan Bosch <joanboschpons@gmail.com>
2024-07-10 13:16:16 +0200

    Working on providers

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+// import { LanguageModal } from "../modals/language-modal";
+
+interface IModalProviderProps {
+  dictionary?: any;
+}
+
+export const ModalProvider = (props: IModalProviderProps) => {
+  const { dictionary } = props;
+  return (
+    <>
+      {/* <LanguageModal /> */}
+    </>
+  );
+};
---
+++ components/sign-in-button.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useSession, signIn } from "next-auth/react";
+import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
+import config from "@/config";
+import { ChevronRight } from "lucide-react";
+
+// A simple button to sign in with our providers (Google & Magic Links).
+// It automatically redirects user to callbackUrl (config.auth.callbackUrl) after login, which is normally a private page for users to manage their accounts.
+// If the user is already logged in, it will show their profile picture & redirect them to callbackUrl immediately.
+const SignInButton = ({
+  text = "Sign In",
+  extraStyle,
+}: {
+  text?: string;
+  extraStyle?: string;
+}) => {
+  const router = useRouter();
+  const params = useParams();
+  const { data: session, status } = useSession();
+
+  const handleClick = () => {
+    if (status === "authenticated") {
+      router.push(`/${params.lang}${config.auth.callbackUrl}`);
+    } else {
+      signIn(undefined, { callbackUrl: `/${params.lang}${config.auth.callbackUrl}` });
+    }
+  };
+
+  if (status === "authenticated") {
+    return (
+      <Link
+        href={`/${params.lang}${config.auth.callbackUrl}`}
+        className={`bg-white rounded-xl flex items-center  ${extraStyle ? extraStyle : ""}`}
+      >
+        {`Dashboard `}
+        <ChevronRight size={16} />
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      className={`bg-white rounded-xl flex items-center ${extraStyle ? extraStyle : ""}`}
+      onClick={handleClick}
+    >
+      {`Sign In `}
+      <ChevronRight size={16} />
+    </button>
+  );
+};
+
+export default SignInButton;
