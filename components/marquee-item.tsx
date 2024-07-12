220fb5060f1e56dafb6d8e684f3ee8849a5aebee
Joan Bosch <joanboschpons@gmail.com>
2024-07-12 16:27:05 +0200

    Add marquee item

---
+++ components/marquee-item.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils"
+
+interface AwesomeCardIconProps extends React.HTMLAttributes<HTMLDivElement> {
+  icon: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement>> | ((props: React.HTMLAttributes<SVGElement>) => React.SVGProps<SVGSVGElement>)
+  strokeWidth?: number
+}
+
+export default function MarqueeItem({
+  icon,
+  children,
+  className,
+  strokeWidth = 1,
+  ...props
+}: AwesomeCardIconProps) {
+  let Icon = icon as React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement>>
+  return (
+    <div className={cn(`flex items-center content-center justify-center p-2 rounded-xl bg-muted aspect-square`, className)} {...props}>
+      <Icon className="h-full w-full" strokeWidth={strokeWidth}/>
+    </div>
+  )
+}
